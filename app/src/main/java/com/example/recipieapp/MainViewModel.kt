90f02fe301eb8aecb23f34477e3b969ce3cebe27
hennan1 <120299739+hennan1@users.noreply.github.com>
2024-05-15 23:10:16 +0500

    implemented fetchCategories for fetching the details of categories

--- app/src/main/java/com/example/recipieapp/MainViewModel.kt
+++ app/src/main/java/com/example/recipieapp/MainViewModel.kt
@@ -3,11 +3,35 @@ package com.example.recipieapp
 import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
 
 class MainViewModel:ViewModel() {
 
     private val _categoriesState = mutableStateOf(recipieState())
     val categoriesState:State<recipieState> = _categoriesState
+
+    fun fetchCategory(){
+        //The coroutine is the background that keeps on running while
+        //suspend function does its work
+        viewModelScope.launch {
+            try {
+                val response = recipieService.getCategories()
+                _categoriesState.value = _categoriesState.value.copy(
+                    list = response.categoriesList,
+                    loading = false,
+                    error = null,
+
+                )
+
+            }catch (e:Exception){
+                _categoriesState.value = _categoriesState.value.copy(
+                    loading = false,
+                    error = "Error fetching the category"
+                )
+            }
+        }
+    }
 
     data class recipieState(
         val loading : Boolean = true,
